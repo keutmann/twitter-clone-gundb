41f194f0a8a777760fe7b5d1ec70cfdc6c16ca24
Carsten Keutmann <keutmann@yahoo.com>
2021-05-18 10:58:33 +0200

    Feedload logic updated.

--- src/components/FeedList.js
+++ src/components/FeedList.js
@@ -12,31 +12,76 @@ const Wrapper = styled.div`
 
 const FeedList = () => {
 
-  const { feed, feedReady, setFeed, resetFeedReady, messageReceived } = useUser();
+  const { feed, feedReady, setFeed, resetFeedReady } = useUser(); // messageReceived
   //const [ items, setItems ] = useState();
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    const items = Object.values(feedReady);
-    if(!feed) {
-      setFeed([...items]); // Simply copy ready feed, more advanced sorting on date etc. may be implemented.
-      resetFeedReady();
-      console.log("FeedList Initialized");
-    } 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[messageReceived]);
-
-
-  // // eslint-disable-next-line react-hooks/rules-of-hooks
   // useEffect(() => {
   //   const items = Object.values(feedReady);
-  //   if(feed && items.length > 0) {
-  //     setFeed([...items, ...feed]); // Simply copy ready feed, more advanced sorting on date etc. may be implemented.
+  //   if(!feed) {
+  //     setFeed([...items]); // Simply copy ready feed, more advanced sorting on date etc. may be implemented.
   //     resetFeedReady();
-  //     console.log("FeedList setFeed");
-  //   }
+  //     console.log("FeedList Initialized");
+  //   } 
   // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
+  // },[messageReceived]);
+
+
+  // Events
+  // Page load, effect run
+  // Message comes in
+  // result : empty list
+  // Want: Waiting loader.... for 5 secords.
+
+  // Solution timeout!
+  // Run effect - result empty, then rerun 5 times 1 sec interval.
+
+  // Start the effect on page load
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+
+    const loadFeed = () => {
+      let temp = feed || [];
+
+      const items = Object.values(feedReady);
+      if(items.length > 0) {
+        setFeed([...items, ...temp]); // Simply copy ready feed, more advanced sorting on date etc. may be implemented.
+        resetFeedReady();
+        console.log("FeedList setFeed");
+        return true;
+      }
+
+      if(feed)  // No new messages but we have a feed already
+        return true;
+
+      return false;
+    }
+
+    if(loadFeed()) // Check now for messages
+      return;
+
+    // We have an null feed and no messages, wait 5 seconds for messages.
+    let count = 5;
+    let timerId = setInterval(() => {
+      console.log("LoadFeed interval " + count);
+      
+      if(loadFeed()) { // Load new messages
+        clearInterval(timerId);
+        return;
+      }
+
+      count--;
+      if(count === 0) { // Feed is null, and no messages found, make empty feed.
+        setFeed([]);
+        clearInterval(timerId);
+        return;
+      }
+
+    }, 1000);
+
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   if(!feed) return <Loader />;
   console.log("FeedList render");
