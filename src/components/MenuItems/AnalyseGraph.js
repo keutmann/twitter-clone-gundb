6e5808ad8ab09c5b9205524bf8cb0af4ea2ae6da
Carsten Keutmann <keutmann@yahoo.com>
2021-06-14 12:54:07 +0200

    Renaming

--- src/components/MenuItems/AnalyseGraph.js
+++ src/components/MenuItems/AnalyseGraph.js
@@ -1,20 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import MenuButton from "../../styles/MenuButton";
 import { ThemeContext } from "../../context/ThemeContext";
 import { Diagram2 } from 'react-bootstrap-icons';
+import AnalyseGraphModal from "../Analyse/AnalyseGraphModal";
 
-const AnalyseGraph = ({ user }) => {
+const AnalyseGraph = ({ user, popupClose }) => {
 
   const { theme } = useContext(ThemeContext);
 
-  const handleClick = () => {
-
+  const [open, setOpen] = useState(false);
+  const openModal = () => {
+    setOpen(true);
   }
+  const closeModal = () => {
+    setOpen(false);
+    popupClose();
+  }
+  const saveModal = (obj) => { 
+      console.log(obj); 
+      closeModal();
+    }
+
 
   return (
-    <MenuButton onClick={handleClick}>
+    <MenuButton onClick={openModal}>
       <Diagram2 color={theme.accentColor} />
       <p>Analyse connections</p>
+      <AnalyseGraphModal open={open} onClose={closeModal} saveModal={saveModal} header={"Analyse user"} saveTitle={"Save"} />
     </MenuButton>
   );
 };
